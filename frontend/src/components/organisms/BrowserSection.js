129256f7ec233ab19372cf171bf835bf57fe0a66
Rafal Cybula <242370@edu.p.lodz.pl>
2024-05-26 17:44:35 +0200

    Added search button

--- frontend/src/components/organisms/BrowserSection.js
+++ frontend/src/components/organisms/BrowserSection.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import '../../styles/organisms/BrowserSection.css';
 import Browser from "./Browser";
+import ButtonPrimary from "../atoms/ButtonPrimary";
 const BrowserSection = () =>
 {
+    const searchButtonText = "Szukaj";
+
     return(
       <div className="BrowserSection">
           <Browser/>
+          <ButtonPrimary value={searchButtonText}/>
       </div>
     );
 }
