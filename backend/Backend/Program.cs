08d101d9310d61dd435294602be9fe83a9f8169f
Rafal Cybula <242370@edu.p.lodz.pl>
2024-06-08 23:28:37 +0200

    Disabled CORS

--- backend/Backend/Program.cs
+++ backend/Backend/Program.cs
@@ -4,11 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     builder.Services.AddSingleton<ITripService, TripService>();
+    builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 }
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
     app.MapControllers();
+    app.UseCors("AllowAll");
     app.Run();
 }
